20fc1882d27e514a74d288de106c3bd3556a8892
swayamg26 <swayamg78@gmail.com>
2025-08-16 11:19:26 +0530

    Add files via upload

---
+++ conj.js
@@ -0,0 +1,238 @@
+window.comboMeanings = {
+"12":`The effect of Sun will be more than Moon
+The effect of Moon will be more than Sun
+Govt related issues sometime saving after realization
+Spendthrift person , health issues /attractive face
+Educated & financially stable possible in debt
+Sensitive and helpful people attractive`,
+"21":`The effect of Moon will be more than Sun
+The effect of Sun will be more than Moon
+Govt related issues sometime saving after realization
+Spendthrift person , health issues /attractive face
+Educated & financially stable possible in debt
+Sensitive and helpful people attractive`,
+"13":`The effect of Sun will be more than Jupiter
+The effect of Jupiter will be more than Sun
+Fame can be Dr /Judge .Self made Persion
+Person has good education ( occult lover ) &
+does reputed Work like boss , Dr ,Judge
+The person has extreme experience /education
+Mostly works in Govt like civil services
+Good adviser
+Good health and marriage life`,
+"31":`The effect of Jupiter will be more than Sun
+The effect of Sun will be more than Jupiter
+Fame can be Dr /Judge .Self made Persion
+Person has good education ( occult lover ) &
+does reputed Work like boss , Dr ,Judge
+The person has extreme experience /education
+Mostly works in Govt like civil services
+Good adviser
+Good health and marriage life`,
+"14":`The effect of Sun will be more than Rahu
+The effect of Rahu will be more than Sun
+Secret enemies, Issues in Govt related issue , Possibility of Sudden losses health issue
+Defame & loss of money debt
+Family relation not Good , Disharmony in relation`,
+"41":`The effect of Rahu will be more than Sun
+The effect of Sun will be more than Rahu
+Secret enemies, Issues in Govt related issue , Possibility of Sudden losses health issue
+Defame & loss of money debt
+Family relation not Good , Disharmony in relation`,
+"15":`The effect of Sun will be more than Mercury
+Successful in exams, Job
+Support from father and their father get pride by these people and Good for job Helpful in Marrige`,
+"51":`The effect of Mercury will be more than Sun
+Successful in exams, Job
+Support from father and their father get pride by these people and Good for job Helpful in Marrige`,
+"16":`The effect of Sun will be more than Venus
+Spouce health issues Money & frequent issue in job / job loss less income
+Causes UTI problems or piles
+Disturbs relation with all people`,
+"61":`The effect of Venus will be more than Sun
+Spouce health issues Money & frequent issue in job / job loss less income
+Causes UTI problems or piles
+Disturbs relation with all people`,
+"17":`The effect of Sun will be more than Ketu
+Government /Good jobs or persion in relation have govt job , but not able to give proper time to Family so ther could be some family issues , they have a feeling that by lying they get more profit
+Leadership Qualities their could be chances of two marriage or relation`,
+"71":`The effect of Ketu will be more than Sun
+Government /Good jobs or persion in relation have govt job , but not able to give proper time to Family so ther could be some family issues , they have a feeling that by lying they get more profit
+Leadership Qualities their could be chances of two marriage or relation`,
+"18":`The effect of Sun will be more than Saturn
+Possibility of father loss /Difference between father and child
+Spouce health issue , frequent job change or job relatedissue`,
+"81":`The effect of Saturn will be more than Sun
+Possibility of father loss /Difference between father and child
+Spouce health issue , frequent job change or job relatedissue`,
+"19":`The effect of Sun will be more than Mars
+Get unearned money and own what you want
+Highly educated , Possible HOD of work Place
+Independent workers ( Surgeon or Engineer )`,
+"91":`The effect of Mars will be more than Sun
+Get unearned money and own what you want
+Highly educated , Possible HOD of work Place
+Independent workers ( Surgeon or Engineer )`,
+"23":`The effect of Moon will be more than Jupiter
+Parents will be embarrassed because of their child
+Many enemies but Win over enemies , Education with hurdles can be of aggressive nature
+Extra martial connection`,
+"32":`The effect of Jupiter will be more than Moon
+Parents will be embarrassed because of their child
+Many enemies but Win over enemies , Education with hurdles can be of aggressive nature
+Extra martial connection`,
+"24":`The effect of Moon will be more than Rahu
+Negative minded Thought , Can have struggle in Family & Family think he gives trouble only`,
+"42":`The effect of Rahu will be more than Moon
+Moody , Can be attracted to bad Company
+Gives trouble to family can patience to achieve something`,
+"25":`The effect of Moon will be more than Mercury
+Very intelligent , Sharp Mind Persion Interested in occult science Air travel is successful
+Blessed with magical powers, Can be trapped in Finacial elated Issues`,
+"52":`The effect of Mercury will be more than Moon
+Very intelligent , Sharp Mind Persion Interested in occult science Air travel is successful
+Blessed with magical powers, Can be trapped in Finacial elated Issues`,
+"26":`The effect of Moon will be more than Venus
+Hurdles in education
+Super convincing powers , May face extra relation issue in life
+Family issues Girl face issue with mother in law`,
+"62":`The effect of Venus will be more than Moon
+Hurdles in education
+Super convincing powers , May face extra relation issue in life
+Family issues Girl face issue with mother in law`,
+"27":`The effect of Moon will be more than Ketu
+Joint pain , urine infection
+Problems in profession`,
+"72":`The effect of Ketu will be more than Moon
+Joint pain , urine infection
+Problems in profession`,
+"28":`The effect of Moon will be more than Saturn
+Aggressive in nature , Mood Swing ,Two marriage in Family`,
+"82":`The effect of Saturn will be more than Moon
+Partnership not suitable, Want to all things quickely
+Depression, Can be indulge in bad habit or Addiction Quickely`,
+"29":`The effect of Moon will be more than Mars
+Very good financial status Live happily on others money`,
+"92":`The effect of Mars will be more than Moon
+Self earned money ,may be egoistic`,
+"34":`The effect of Jupiter will be more than Ketu
+Child will not stay with parents
+Breathing issues, leg shivering , Some one in family may be paralysed`,
+"43":`The effect of Ketu will be more than Jupiter
+Child will not stay with parents
+Breathing issues, leg shivering , Some one in family may be paralysed`,
+"35":`The effect of Jupiter will be more than Mercury
+Sharp Mind ,Financial Problems although will have good fund flow
+Have to leave parents property and can not enjoy their own asset Success away from family`,
+"53":`The effect of Mercury will be more than Jupiter
+Sharp Mind ,Financial Problems although will have good fund flow
+Have to leave parents property and can not enjoy their own asset Success away from family`,
+"36":`The effect of Jupiter will be more than Venus
+Multi-talented
+Religious and spiritual inclined growth after marriage barrier in education
+Attitude Father & Son not live together, they will make their own rule and regulation & self respect is important`,
+"63":`The effect of Venus will be more than Jupiter
+Multi-talented
+Religious and spiritual inclined growth after marriage barrier in education
+Attitude Father & Son not live together, they will make their own rule and regulation & self respect is important`,
+"37":`The effect of Jupiter will be more than Ketu
+Bring you to the top position in your respective field
+Financial support , personal would able to take benefit from knowledge
+They will cause no harm`,
+"73":`The effect of Ketu will be more than Jupiter
+Bring you to the top position in your respective field
+Financial support , personal would able to take benefit from knowledge
+They will cause no harm`,
+"38":`The effect of Jupiter will be more than Saturn
+Property and sales field would be beneficial , Achivement after hardship`,
+"83":`The effect of Saturn will be more than Jupiter
+Property and sales field would be beneficial , Achivement after hardship`,
+"39":`The effect of Jupiter will be more than Mars
+Love to Show off , Good Debater , Hard work less result person may have lots of duality in nature`,
+"93":`The effect of Mars will be more than Jupiter
+Love to Show off , Good Debater , Hard work less result person may have lots of duality in nature`,
+"45":`The effect of Rahu will be more than Mercury
+Intelligent & wise, May trapped /involve in court issues
+Less eyesight, Stress
+Bandhan Dosh, May also an regular visitor of Hospital/court /medication etc`,
+"54":`The effect of Mercury will be more than Rahu
+Intelligent & wise, May trapped /involve in court issues
+Less eyesight, Stress
+Bandhan Dosh, May also an regular visitor of Hospital/court /medication etc`,
+"46":`The effect of Rahu will be more than Venus
+Skin Disease & Piles, Can be in bad company
+/Can have revenge type of thought , Criminal tendencies , Negative Thought about about
+apposite gender specially for Female Could be rape Victim or Doer also`,
+"64":`The effect of Venus will be more than Rahu
+Skin Disease & Piles, Can be in bad company
+/Can have revenge type of thought , Criminal tendencies , Negative Thought about about
+apposite gender specially for Female Could be rape Victim or Doer also`,
+"47":`The effect of Rahu will be more than Ketu
+Clever Personality
+Service Provider
+Strong determination & will power`,
+"74":`The effect of Ketu will be more than Rahu
+Clever Personality
+Service Provider
+Strong determination & will power`,
+"48":`The effect of Rahu will be more than Saturn
+Accident Prone Persion ,Depression & Stress, Lack bed comfort`,
+"84":`The eff ect of Saturn will be more than Rahu
+Accident Prone Persion ,Depression & Stress, Lack bed comfort`,
+"49":`The effect of Rahu will be more than Mars
+Risky jobs are taken up and most of the time under stress for their job/work
+Success after hard work`,
+"94":`The eff ect of Mars will be more than Rahu
+Risky jobs are taken up and most of the time under stress for their job/work
+Success after hard work`,
+"56":`The effect of Mercury will be more than Venus
+Failure in Love issue as they can not express their feeling They cannot ask for their own money as they are very shy`,
+"65":`The eff ect of Venus will be more than Mercury
+Failure in Love issue as they can not express their feeling They cannot ask for their own money as they are very shy`,
+"57":`The effect of Mercury will be more than Ketu
+Good public speaker
+Carry quality of true business man
+Can be writer and astrologer`,
+"75":`The effect of Ketu will be more than Mercury
+Good public speaker
+Carry quality of true business man
+Can be writer and astrologer`,
+"58":`The effect of Mercury will be more than Saturn
+Heavy financial loss ,Property and money are blocked for further Growth`,
+"85":`The effect of Saturn will be more than Mercury
+Heavy financial loss ,Property and money are blocked for further Growth`,
+"59":`The effect of Mercury will be more than Mars
+Sharp minded people
+Immense technical knowledge
+Successful businessman ,Straight forward people so judged as rude ones`,
+"95":`The effect of Mars will be more than mrcury
+Sharp minded people
+Immense technical knowledge
+Successful businessman ,Straight forward people so judged as rude ones`,
+"68":`The effect of Venus will be more than Saturn
+Suitable for surgeons, doctors and people who work in hospitals
+Health problems`,
+"86":`The effect of Saturn will be more than Venus
+Suitable for surgeons, doctors and people who work in hospitals
+Health problems`,
+"69":`The effect of Venus will be more than Mars
+Creative mind, Suitable for event management, marriage planners, designers Generally work with opposite sex as they don't get well with the same gender`,
+"96":`The effect of Mars will be more than Venus
+Creative mind, Suitable for event management, marriage planners, designers Generally work with opposite sex as they don't get well with the same gender`,
+"78":`The effect of Ketu will be more than Saturn
+Spiritual healer, People with negative thought process , Loneliness, Dipression Bad habit`,
+"87":`The effect of Saturn will be more than Ketu
+Spiritual healer, People with negative thought process , Loneliness, Dipression Bad habit`,
+"79":`The effect of Ketu will be more than Mars
+Up down in career , If away from father will have a better life
+Blood related issues/joint pains/kidney problems`,
+"97":`The eff ect of Mars will be more than Ketu
+Up down in career , If away from father will have a better life
+Blood related issues/joint pains/kidney problems`,
+"89":`Effect of Saturn will be more than Mars
+Effect of Mars will be more than Saturn
+Aggression, Stuggle in life health issues , Astrologer , Service Provider ,Advocate , Stock Brocker`,
+"98":`Effect of Saturn will be more than Mars
+Effect of Mars will be more than Saturn
+Aggression, Stuggle in life health issues , Astrologer , Service Provider ,Advocate , Stock Brocker`
+};
